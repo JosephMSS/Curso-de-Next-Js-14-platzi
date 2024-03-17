a2a4a222a4baa3afd4b29748c66a6203baf05cb8
Joseph Morales Silva <josephzx1@hotmail.com>
2024-03-16 20:11:42 -0600

    Add classnames dependency and update Description component

--- next-store/src/components/Home/Description/Description.tsx
+++ next-store/src/components/Home/Description/Description.tsx
@@ -1,19 +1,37 @@
+'use client';
+import classnames from "classnames/bind";
 import Image from "next/image";
+import { useState } from "react";
 import styles from "./Description.module.sass";
 import { PLACEHODER_IMAGE } from "./models";
-
 export const Description = () => {
+  const [hasBorder, setHasBorder] = useState(false)
+
+  /**
+   * Provide context to the classnames library
+   * enable to use the hash classnames from the styles module
+   */
+  const cx = classnames.bind(styles);
+  const buttonStyles = cx("Description__button", {
+    "Description__button--border": hasBorder
+  })
+  const handleClick = () => {
+    setHasBorder(!hasBorder);
+  }
   return (
     <section className={styles.Description}>
-      <div className={styles.Description__imageContainer}>
+      <button onClick={handleClick} className={buttonStyles}>
+        <div className={styles.Description__imageContainer}>
 
-        <Image src="/images/description.jpeg" alt="products marketplace"
-          priority={false}
-          fill={true}
-          placeholder="blur"
-          blurDataURL={PLACEHODER_IMAGE}
-        />
-      </div>
+          <Image src="/images/description.jpeg" alt="products marketplace"
+            priority={false}
+            fill={true}
+            placeholder="blur"
+            blurDataURL={PLACEHODER_IMAGE}
+          />
+        </div>
+      </button>
+
       <div className={styles.Description__text}>
         <h2>Bring the future today</h2>
         <p>
